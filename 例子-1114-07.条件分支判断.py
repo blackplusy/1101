a1c6e5dd76c2c9d44a5014ca92846ef915bf72cd
blackplusy <osimeno@163.com>
2019-11-14 14:34:58 +0800

    Add files via upload

---
+++ 例子-1114-07.条件分支判断.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+# 1.单分支
+# name = input('请输入您大表哥的名字:')
+# if name=='王思聪':
+#     print('IG牛逼！！！！')
+#2.双分支
+# sal=int(input('请输入您的工资：'))
+# if sal>=10000:
+#     print('嘿嘿嘿~')
+# else:
+#     print('洗洗睡吧！')
+#3.多分支
+name=input('请输入您喜欢的主播：')
+if name=='大司马':
+    print('三角形打野！')
+elif name=='张大仙':
+    print('不慌，吃口药')
+elif name=='55开':
+    print('难受啊马飞！')
+else:
+    print('您说的主播不存在，请到斗鱼抖索黑哥GaGa')
+
+
+
+
